bfb5cb9adae8e52df4927def3020165e9698181d
Sean Wines <sean.wines@vuria.com>
2017-07-15 18:51:31 -0700

    Created landing banner with parallax

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,9 @@
-var emailIcon = document.querySelector('.fa-envelope');
-var emailContainer = document.querySelector('.email-container');
-var closeEmailContainer = document.querySelector('.fa-close');
+var cloudsLayer = document.querySelector('.clouds-layer');
 
-emailIcon.addEventListener('click', function () {
-  emailContainer.classList.toggle('show-email');
-})
-
-closeEmailContainer.addEventListener('click', function () {
-  emailContainer.classList.toggle('show-email');
+window.addEventListener('scroll', function () {
+  let pageLocation = -(window.pageYOffset / 4);
+  let cloudScroll = pageLocation + 'px';
+  if (pageLocation > -180) {
+    cloudsLayer.style.marginTop = cloudScroll;
+  }
 })
